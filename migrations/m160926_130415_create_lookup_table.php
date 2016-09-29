5c792c7620d1670afbf313fe3dfca906fcdc1d1c
Krassowsky <egorkryazh@gmail.com>
2016-09-29 14:57:28 +0800

    Post statuses to Lookup table migration added

--- migrations/m160926_130415_create_lookup_table.php
+++ migrations/m160926_130415_create_lookup_table.php
@@ -19,6 +19,30 @@ class m160926_130415_create_lookup_table extends Migration
             'type' => $this->string(20),
             'position' => $this->integer(),
         ]);
+        
+        $this->insert('lookup', [
+	        'id' => 1,
+	        'name' => 'Draft',
+	        'code' => 1,
+	        'type' => 'PostStatus',
+	        'position' => 1
+        ]);
+
+        $this->insert('lookup', [
+	        'id' => 2,
+	        'name' => 'Published',
+	        'code' => 2,
+	        'type' => 'PostStatus',
+	        'position' => 2
+        ]);
+
+        $this->insert('lookup', [
+	        'id' => 3,
+	        'name' => 'Archived',
+	        'code' => 3,
+	        'type' => 'PostStatus',
+	        'position' => 3
+        ]);
     }
 
     /**
