4b54840208cfc2a4993b1f7b8afc71508d9e7cd0
Krassowsky <egorkryazh@gmail.com>
2016-10-12 13:59:52 +0800

    TinyMce for post editor added. Status code field fixed.

--- modules/blog/views/admin/post/_form.php
+++ modules/blog/views/admin/post/_form.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use app\models\Lookup;
+use dosamigos\tinymce\TinyMce;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Post */
@@ -13,11 +14,21 @@ use app\models\Lookup;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'title')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'title')->textarea(['rows' => 1]) ?>
 
-    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'status')->dropDownList(Lookup::items('PostStatus')) ?>
+	<?= $form->field($model, 'content')->widget(TinyMce::className(), [
+	    'options' => ['rows' => 6],
+	    'language' => 'ru',
+	    'clientOptions' => [
+	        'plugins' => [
+	            "advlist autolink lists link code searchreplace insertdatetime contextmenu paste image pagebreak"
+	        ],
+	        'menubar' => false,
+	        'toolbar' => "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist | paste link image | pagebreak | code"
+	    ]
+	]);?>
+	
+    <?= $form->field($model, 'status_code')->dropDownList(Lookup::items('PostStatus')) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
